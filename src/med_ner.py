131f32ae945713b6cee18d300044d35a691f157b
yunyu <yunyuhu852@gmail.com>
2023-06-28 17:41:08 +0800

    update: add medicine ID from other website

--- src/med_ner.py
+++ src/med_ner.py
@@ -19,24 +19,40 @@ def find_medicine(sentence):
     return med_ner
 
 
-def take_drug_name(durg_info):
+def take_drug_id(durg_info):
 
     drug_name = []
     drugbank_id = []
+    medline_plus_id = []
+    mesh_id = []
+    nhs_url = []
+    wikipedia_url = []
 
     try:
         for item in durg_info:
             drug_name.append(item[0]['name'])
             drugbank_id.append(item[0]['drugbank_id'])
+            medline_plus_id.append(item[0]['medline_plus_id'])
+            mesh_id.append(item[0]['mesh_id'])
+            nhs_url.append(item[0]['nhs_url'])
+            wikipedia_url.append(item[0]['wikipedia_url'])
     except:
         drug_name = 'None'
         drugbank_id = 'None'
+        medline_plus_id = 'None'
+        mesh_id = 'None'
+        nhs_url = 'None'
+        wikipedia_url = 'None'
 
     if len(drug_name) == 0:
         drug_name = 'None'
         drugbank_id = 'None'
+        medline_plus_id = 'None'
+        mesh_id = 'None'
+        nhs_url = 'None'
+        wikipedia_url = 'None'
 
-    return drug_name, drugbank_id
+    return drug_name, drugbank_id, medline_plus_id, mesh_id, nhs_url, wikipedia_url
 
 
 def main():
@@ -45,19 +61,32 @@ def main():
 
     drug_name_list = []
     drugbank_id_list = []
+    medline_plus_id_list = []
+    mesh_id_list = []
+    nhs_url_list = []
+    wikipedia_url_list = []
 
     for i in df['text']:
         durg_info = find_medicine(i)
-        drug_name, drugbank_id = take_drug_name(durg_info)
+        drug_name, drugbank_id, medline_plus_id, mesh_id, nhs_url, wikipedia_url = take_drug_id(
+            durg_info)
         drug_name_list.append(drug_name)
         drugbank_id_list.append(drugbank_id)
+        medline_plus_id_list.append(medline_plus_id)
+        mesh_id_list.append(mesh_id)
+        nhs_url_list.append(nhs_url)
+        wikipedia_url_list.append(wikipedia_url)
 
     # add drug name and drugbank id
     df['drug_name'] = drug_name_list
     df['drugbank_id'] = drugbank_id_list
+    df['medline_plus_id'] = medline_plus_id_list
+    df['mesh_id'] = mesh_id_list
+    df['nhs_url'] = nhs_url_list
+    df['wikipedia_url'] = wikipedia_url_list
 
     # save to csv
-    df.to_csv('data/preproc/data_with_drug.csv')
+    df.to_csv('data/preproc/preproc_en_train.csv')
 
 
 if __name__ == '__main__':
