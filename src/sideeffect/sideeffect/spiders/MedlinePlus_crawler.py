e34501c379672eef2c0e8a27e774b77e80c085c8
yunyu <yunyuhu852@gmail.com>
2023-06-29 14:18:25 +0800

    update: crawl html file

--- src/sideeffect/sideeffect/spiders/MedlinePlus_crawler.py
+++ src/sideeffect/sideeffect/spiders/MedlinePlus_crawler.py
@@ -1,21 +1,43 @@
 from pathlib import Path
-
+import pandas as pd
 import scrapy
+import ast
+
+DATA_FILE = "../data/preproc/preproc_en_train.csv"
 
 
 class QuotesSpider(scrapy.Spider):
     name = "MedlinePlus"
 
     def start_requests(self):
-        urls = [
-            "https://medlineplus.gov/druginfo/meds/a687009.html",
-            "https://medlineplus.gov/druginfo/meds/a614007.html",
-        ]
+
+        df = pd.read_csv(DATA_FILE)
+        drug_id = df['medline_plus_id'].tolist()
+
+        drug_id_list = []
+        urls = []
+
+        for item in drug_id:
+
+            l = ast.literal_eval(item)
+            l = [item.strip() for item in l]
+
+            for i in l:
+                # print(i)
+                if i == 'None':
+                    continue
+                drug_id_list.append(i)
+            drug_id_list = list(set(drug_id_list))
+            drug_id_list.sort()
+
+        for i in drug_id_list:
+            urls.append(f"https://medlineplus.gov/druginfo/meds/{i}.html")
+
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         drug_id = response.url.split("/")[-1][:-5]
-        filename = f"MedlinePlus-{drug_id}.html"
+        filename = f"../data/crawler/MedlinePlus/MedlinePlus-{drug_id}.html"
         Path(filename).write_bytes(response.body)
         self.log(f"Saved file {filename}")
