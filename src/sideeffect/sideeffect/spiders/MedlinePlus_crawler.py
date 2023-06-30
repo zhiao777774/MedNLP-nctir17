ee2fe1b9e197f701f37c986f874e004ee02d877d
yunyu <yunyuhu852@gmail.com>
2023-06-30 10:33:08 +0800

    update: add side_effects from medlineplus

--- src/sideeffect/sideeffect/spiders/MedlinePlus_crawler.py
+++ src/sideeffect/sideeffect/spiders/MedlinePlus_crawler.py
@@ -37,7 +37,28 @@ class MedlinePlusSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        drug_id = response.url.split("/")[-1][:-5]
-        filename = f"../data/crawler/MedlinePlus/MedlinePlus-{drug_id}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+
+        # get name only from xpath
+        drug_name = response.xpath(
+            '//*[@id="mplus-content"]/article/div[1]/div[1]/h1/text()').get()
+
+        # get description only from xpath
+        normal_description = response.xpath(
+            '//*[@id="section-side-effects"]/ul[1]/li/text()').getall()
+        serious_description = response.xpath(
+            '//*[@id="section-side-effects"]/ul[2]/li/text()').getall()
+
+        item = {
+            drug_name: {
+                'normal': normal_description,
+                'serious': serious_description
+            }
+        }
+
+        yield item
+
+        # print(item)
+        # drug_id = response.url.split("/")[-1][:-5]
+        # filename = f"../data/crawler/MedlinePlus/MedlinePlus-{drug_id}.html"
+        # Path(filename).write_bytes(response.body)
+        # self.log(f"Saved file {filename}")
