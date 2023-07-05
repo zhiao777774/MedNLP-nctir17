e49528fd867c2140835a64fdafd27f103ec3d090
zhiao777774 <zhiaohsu@gmail.com>
2023-07-05 15:05:48 +0800

    refactor: Refactored the code to improve data loading and access for searching drug side effects

--- src/utils/util.py
+++ src/utils/util.py
@@ -1,6 +1,22 @@
 import json
 from types import Dict, List
 
+from path_utils import SIDE_EFFECTS_DATA_FOLDER
+
+SIDE_EFFECTS_DATA = None
+
+
+def load_side_effects_data():
+    global SIDE_EFFECTS_DATA
+    with open(SIDE_EFFECTS_DATA_FOLDER / 'medlineplus_side_effects.json',
+              'r') as f:
+        loaded_side_effects = json.load(f)
+
+    SIDE_EFFECTS_DATA = {
+        k.lower(): v
+        for item in loaded_side_effects for k, v in item.items()
+    }
+
 
 def search_side_effects(drug: str) -> Dict[str, List[str]]:
     """Search for side effects of a given drug.
@@ -11,14 +27,17 @@ def search_side_effects(drug: str) -> Dict[str, List[str]]:
     Returns:
         Dict[str, List[str]]: Side effects of the given drug.
     """
+    global SIDE_EFFECTS_DATA
+    if not SIDE_EFFECTS_DATA:
+        load_side_effects_data()
 
-    with open('src/data/sideeffects/medlineplus_side_effects.json', 'r') as f:
-        tmp_side_effect = json.load(f)
-    side_effect = {
-        k.lower(): v
-        for item in tmp_side_effect for k, v in item.items()
+    drug_name = drug.lower()
+    matches_side_effects = [
+        v for k, v in SIDE_EFFECTS_DATA.items() if drug_name in k
+    ]
+    side_effects = matches_side_effects[0] if matches_side_effects else {
+        'normal': [],
+        'serious': []
     }
-    drug = drug.lower()
-    side_effect = {k: v for k, v in side_effect.items() if drug in k}
 
-    return side_effect
+    return side_effects
